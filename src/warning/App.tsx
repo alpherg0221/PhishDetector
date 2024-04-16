e5260ecb233b7d841f473b9bdd7e9eb304d4ca06
yuki <ywatanabe6229@gmail.com>
2024-04-16 15:33:15 +0900

    Added English warning page

--- src/warning/App.tsx
+++ src/warning/App.tsx
@@ -2,6 +2,8 @@ import { DefaultButton, Stack, Text, ThemeProvider } from "@fluentui/react";
 import "./App.css"
 
 const App = () => {
+  const isEnglish = window.navigator.language === "en";
+
   const closePage = () => chrome.runtime.sendMessage({ type: "close" });
 
   return (
@@ -17,18 +19,21 @@ const App = () => {
             <img src="/warning.svg" alt="WarnignIcon"/>
             <img src="/phishing.svg" alt="PhishingIcon"/>
           </Stack>
-          <Text style={ { color: "#e9e9e9", fontSize: "2.5em" } }>フィッシングサイトを検出しました</Text>
-          <Text style={ { color: "#e9e9e9", fontSize: "1.25em" } }>
-            <b>{ (new URL(location.href)).searchParams.get("url") }</b> は機密情報を盗み取る疑いがあります
+          <Text style={ { color: "#e9e9e9", fontSize: "2.5em" } }>
+            { isEnglish ? "Phishing site detected" : "フィッシングサイトを検出しました" }
           </Text>
           <Text style={ { color: "#e9e9e9", fontSize: "1.25em" } }>
-            検出理由：<b>{ (new URL(location.href)).searchParams.get("by") }</b>
+            <b>{ (new URL(location.href)).searchParams.get("url") }</b>
+            { isEnglish ? " might steal your personally identifiable information." : " は機密情報を盗み取る疑いがあります" }
           </Text>
-          <Text style={ { color: "#e9e9e9", fontSize: "1.25em" } }>
-            検出時間：<b>{ (new URL(location.href)).searchParams.get("time") }</b> ms
-          </Text>
+          {/*<Text style={ { color: "#e9e9e9", fontSize: "1.25em" } }>*/}
+          {/*  検出理由：<b>{ (new URL(location.href)).searchParams.get("by") }</b>*/}
+          {/*</Text>*/}
+          {/*<Text style={ { color: "#e9e9e9", fontSize: "1.25em" } }>*/}
+          {/*  検出時間：<b>{ (new URL(location.href)).searchParams.get("time") }</b> ms*/}
+          {/*</Text>*/}
           <Stack horizontalAlign="center" style={ { paddingTop: 20 } }>
-            <DefaultButton onClick={ closePage }>ページを閉じる</DefaultButton>
+            <DefaultButton onClick={ closePage }>{ isEnglish ? "Close this page" : "ページを閉じる" }</DefaultButton>
           </Stack>
         </Stack>
       </ThemeProvider>
